bb9ff0f77718113b72a576d2331e06f5e01c2b98
Karim NASRI <a.karim.nasri@gmail.com>
2021-11-21 11:50:21 +0100

    removed the error in user data initialisation

--- terraform/ec2-server-configuration/user_data.sh
+++ terraform/ec2-server-configuration/user_data.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Clone the repository
 sudo git clone https://github.com/nkasri/devops_test.git
 # Install Docker engine
@@ -8,12 +8,12 @@ sudo apt-get install \
     curl \
     gnupg \
     lsb-release
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 cd devops_test || { echo "Failure"; exit 1;}
 
 # Build image
