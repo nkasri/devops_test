26ea66af7d0af653649ef9c266484fd3d8b04c94
Karim NASRI <a.karim.nasri@gmail.com>
2021-11-21 10:56:52 +0100

    added commands to user data

--- terraform/ec2-server-configuration/user_data.sh
+++ terraform/ec2-server-configuration/user_data.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 # Clone the repository
-git clone https://github.com/nkasri/devops_test.git
+sudo git clone https://github.com/nkasri/devops_test.git
 # Install Docker engine
-apt-get update
-apt-get install \
+sudo apt-get update
+sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 cd devops_test || { echo "Failure"; exit 1;}
-docker build . -t pet-clinic
-docker run -d -p 80:5000 pet-clinic
+
+# Build image
+sudo docker build . -t pet-clinic
+# Run the application container
+sudo docker run -d -p 80:5000 pet-clinic
